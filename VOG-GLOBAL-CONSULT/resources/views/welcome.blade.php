e1534ae7b776550aa7577237b6f64743a4fae927
Oghenetega Gbiyede <tega2112@gmail.com>
2024-07-04 00:17:04 +0100

    Projecy setup
    Complete

--- VOG-GLOBAL-CONSULT/resources/views/welcome.blade.php
+++ VOG-GLOBAL-CONSULT/resources/views/welcome.blade.php
@@ -161,7 +161,8 @@
                             </div>
                         </div>
                     </main>
-
+                    <div id=”example”></div>
+<script src="{{ asset('/js/app.js') }}"></script>
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
